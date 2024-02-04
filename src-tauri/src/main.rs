477311b34dc1b3b131b22de572e46230f35e1144
Slushee <slushee@duck.com>
2024-02-04 20:24:53 +0100

    fix formatting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 // use std::{path::Path, time::Duration};
-// 
+//
 // mod src_tauri;
 // use src_tauri::{get_audio_files, h_greedy, random_list, swap};
 // Stuff you'll probably need ^
