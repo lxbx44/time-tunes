e0e86f738292bc61c6dad816444feb3c3f933d18
Slushee <slushee@duck.com>
2024-02-05 18:06:58 +0100

    Stabilize Playlist struct

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -27,37 +27,6 @@ fn is_audio(entry: &DirEntry) -> bool {
     false
 }
 
-/// Recursively retrieves all supported audio files in the given directory and their duration
-///
-/// Supported types are those that [rodio](https://crates.io/crates/rodio) implements by default:
-/// `mp3`, `wav`, `ogg` and `flac`
-///
-/// # Panics
-/// Panics if the audio file can't be recognized, read or is malformed.
-pub fn get_audio_files(path: &Path) -> Vec<(PathBuf, Duration)> {
-    let walker = WalkDir::new(path)
-        .into_iter()
-        .par_bridge()
-        .filter_map(Result::ok);
-    walker
-        .filter(is_audio)
-        .map(|e| {
-            let path = e.path().to_owned();
-            let file_duration = read_from_path(&path).unwrap().properties().duration();
-            (path, file_duration)
-        })
-        .collect()
-}
-
-#[derive(Debug)]
-pub struct Playlist {
-    pub used: Vec<(PathBuf, Duration)>,
-    pub used_duration: Duration,
-    pub unused: Vec<(PathBuf, Duration)>,
-    pub target: Duration,
-    pub rng: ThreadRng,
-}
-
 /// Creates a list with the paths and durations of music files which when combined roughly match
 /// the target duration. A naive approach is used, where random tracks are selected until the total
 /// duration exceds the target time
@@ -83,40 +52,91 @@ pub fn random_list(mut unused: Vec<(PathBuf, Duration)>, target: Duration) -> Pl
     }
 }
 
+/// Recursively retrieves all supported audio files in the given directory and their duration
+///
+/// Supported types are those that [rodio](https://crates.io/crates/rodio) implements by default:
+/// `mp3`, `wav`, `ogg` and `flac`
+///
+/// # Panics
+/// Panics if the audio file can't be recognized, read or is malformed.
+pub fn get_audio_files(path: &Path) -> Vec<(PathBuf, Duration)> {
+    let walker = WalkDir::new(path)
+        .into_iter()
+        .par_bridge()
+        .filter_map(Result::ok);
+    walker
+        .filter(is_audio)
+        .map(|e| {
+            let path = e.path().to_owned();
+            let file_duration = read_from_path(&path).unwrap().properties().duration();
+            (path, file_duration)
+        })
+        .collect()
+}
+
 type Heuristics = fn(Duration, Duration, Duration) -> bool;
 
-/// Attempts to swap the selected audio track `depth` times accortding to the provided heuristics
-///
-/// # Assumptions
-/// - `depth` is an index contained in playlist.unused
-/// - `index` is contained in playlist.used
-/// - `playlist.used_duration` has been correctly calculated
-pub fn swap(mut playlist: Playlist, index: usize, depth: usize, h: Heuristics) -> Playlist {
-    let swap_attempts: Vec<&(PathBuf, Duration)> = playlist
-        .unused
-        .choose_multiple(&mut playlist.rng, depth)
-        .collect();
+pub struct Playlist {
+    used: Vec<(PathBuf, Duration)>,
+    used_duration: Duration,
+    unused: Vec<(PathBuf, Duration)>,
+    target: Duration,
+    rng: ThreadRng,
+}
 
-    let to_swap = &playlist.used.remove(index);
-    playlist.used_duration -= to_swap.1;
+impl Playlist {
+    /// Returns the number of songs in the playlist
+    pub fn used_len(&self) -> usize {
+        self.used.len()
+    }
 
-    let best_song = *swap_attempts.par_iter().reduce(
-        || &to_swap,
-        |best, current| {
-            let old_total = playlist.used_duration + best.1;
-            let new_total = playlist.used_duration + current.1;
+    /// Returns the number of songs in the directory that have not been used
+    pub fn unused_len(&self) -> usize {
+        self.unused.len()
+    }
 
-            if h(old_total, new_total, playlist.target) {
-                return current;
-            }
-            best
-        },
-    );
+    /// Returns a list with the paths of the songs in the playlist
+    ///
+    /// # Panics
+    /// - Panics if a path contains non UTF-8 glyphs
+    pub fn get(&self) -> Vec<String> {
+        self.used
+            .par_iter()
+            .map(|s| s.0.to_str().unwrap().to_owned())
+            .collect()
+    }
 
-    playlist.used_duration += best_song.1;
-    playlist.used.insert(index, best_song.clone());
+    /// Attempts to swap the selected audio track `depth` times accortding to the provided heuristics
+    ///
+    /// # Assumptions
+    /// - `depth` is an index contained in playlist.unused
+    /// - `index` is contained in playlist.used
+    /// - `playlist.used_duration` has been correctly calculated
+    pub fn swap(&mut self, index: usize, depth: usize, h: Heuristics) -> &mut Self {
+        let swap_attempts: Vec<&(PathBuf, Duration)> =
+            self.unused.choose_multiple(&mut self.rng, depth).collect();
 
-    playlist
+        let to_swap = &self.used.remove(index);
+        self.used_duration -= to_swap.1;
+
+        let best_song = *swap_attempts.par_iter().reduce(
+            || &to_swap,
+            |best, current| {
+                let old_total = self.used_duration + best.1;
+                let new_total = self.used_duration + current.1;
+
+                if h(old_total, new_total, self.target) {
+                    return current;
+                }
+                best
+            },
+        );
+
+        self.used_duration += best_song.1;
+        self.used.insert(index, best_song.clone());
+
+        self
+    }
 }
 
 /// Greedy, grabs the track that gets the total time the closest to target
