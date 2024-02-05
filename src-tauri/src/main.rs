2dba5c7c39c79b08049d4f15d7e0544b7963ae1b
Slushee <slushee@duck.com>
2024-02-05 16:47:45 +0100

    Manually rebase Aria's changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-// use std::{path::Path, time::Duration};
-//
-// mod src_tauri;
-// use src_tauri::{get_audio_files, h_greedy, random_list, swap};
-// Stuff you'll probably need ^
+use std::{path::PathBuf, time::Duration};
+
+mod lib;
+use lib::{get_audio_files, h_greedy, random_list, swap};
+
+#[tauri::command]
+fn get_playlist(time: u64, path: &str) -> Vec<String> {
+    let audio_files = get_audio_files(&PathBuf::from(path));
+    let duration = Duration::from_secs(time);
+    let random_list = random_list(audio_files, duration);
+
+    // TODO: Actually implement the playlist generator
+    Vec::new()
+}
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_playlist])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { open } from "@tauri-apps/api/dialog";
 
-
 // hours
 let hUp: HTMLButtonElement | null = document.querySelector('#hours-up');
 let hDown: HTMLButtonElement | null = document.querySelector('#hours-down');
@@ -14,7 +13,9 @@ hUp?.addEventListener('click', () => {
     } else {
         val += 1;
     }
-    hTxt?.setAttribute('value', val.toString());
+    if (hTxt) {
+        hTxt.value = val.toString();
+    }
 });
 
 hDown?.addEventListener('click', () => {
@@ -26,9 +27,10 @@ hDown?.addEventListener('click', () => {
     } else {
         val -= 1;
     }
-    hTxt?.setAttribute('value', val.toString());
+    if (hTxt) {
+        hTxt.value = val.toString();
+    }
 });
-
 
 // minutes
 let mUp: HTMLButtonElement | null = document.querySelector('#minutes-up');
@@ -39,10 +41,22 @@ mUp?.addEventListener('click', () => {
     let val: number | undefined = mTxt?.valueAsNumber;
     if (val === undefined) {
         val = 1;
+    } else if (val === 59) {
+        let new_h_value: number | undefined = hTxt?.valueAsNumber;
+        if (new_h_value === undefined) {
+            new_h_value = 0;
+        }
+        new_h_value += 1;
+        val = 0;
+        if (hTxt) {
+            hTxt.value = new_h_value.toString();
+        }
     } else {
         val += 1;
     }
-    mTxt?.setAttribute('value', val.toString());
+    if (mTxt) {
+        mTxt.value = val.toString();
+    }
 });
 
 mDown?.addEventListener('click', () => {
@@ -54,9 +68,10 @@ mDown?.addEventListener('click', () => {
     } else {
         val -= 1;
     }
-    mTxt?.setAttribute('value', val.toString());
+    if (mTxt) {
+        mTxt.value = val.toString();
+    }
 });
-
 
 // seconds
 let sUp: HTMLButtonElement | null = document.querySelector('#seconds-up');
@@ -65,12 +80,39 @@ let sTxt: HTMLInputElement | null = document.querySelector('#seconds-input');
 
 sUp?.addEventListener('click', () => {
     let val: number | undefined = sTxt?.valueAsNumber;
+
     if (val === undefined) {
         val = 1;
+    } else if (val === 59) {
+        let new_m_value: number | undefined = mTxt?.valueAsNumber;
+
+        if (new_m_value === undefined) {
+            new_m_value = 1;
+        } else if (new_m_value === 59) {
+            let new_h_value: number | undefined = hTxt?.valueAsNumber;
+            if (new_h_value === undefined) {
+                new_h_value = 0;
+            }
+            new_h_value += 1;
+            new_m_value = 0;
+            if (hTxt) {
+                hTxt.value = new_h_value.toString();
+            }
+            val = 0;
+        } else {
+            new_m_value += 1;
+            val = 0;
+        }
+        if (mTxt) {
+            mTxt.value = new_m_value.toString();
+        }
+        val = 0;
     } else {
         val += 1;
     }
-    sTxt?.setAttribute('value', val.toString());
+    if (sTxt) {
+        sTxt.value = val.toString();
+    }
 });
 
 sDown?.addEventListener('click', () => {
@@ -82,20 +124,39 @@ sDown?.addEventListener('click', () => {
     } else {
         val -= 1;
     }
-    sTxt?.setAttribute('value', val.toString());
+    if (sTxt) {
+        sTxt.value = val.toString();
+    }
 });
 
+function preventNonNumericInput(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    inputElement.value = inputElement.value.replace(/\D/g, '0');
+}
+hTxt?.addEventListener('input', preventNonNumericInput);
+mTxt?.addEventListener('input', preventNonNumericInput);
+sTxt?.addEventListener('input', preventNonNumericInput);
 
+
+function no_max_59(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    if (inputElement.valueAsNumber > 59) {
+        inputElement.value = "59";
+    }
+}
+
+mTxt?.addEventListener('input', no_max_59);
+sTxt?.addEventListener('input', no_max_59);
 
 // Open choose folder
-
 let folderButton: HTMLButtonElement | null = document.querySelector('#choose-folder');
 
 let text1: string = "Choose folder";
 let text2: string;
+let musicFolder: string | string[] | null;
 
 folderButton?.addEventListener('click', async () => {
-    const musicFolder = await open({
+    musicFolder = await open({
         multiple: false,
         directory: true,
         filters: [{
@@ -130,44 +191,64 @@ folderButton?.addEventListener('mouseleave', () => {
     }
 });
 
+interface Time {
+    hours: number,
+    minutes: number,
+    seconds: number,
+    to_seconds(): number,
+}
 
+class Time implements Time {
+    constructor(public hours: number, public minutes: number, public seconds: number) {}
 
-// submit form
-
-let mainform: HTMLFormElement | null = document.querySelector('main-form');
-
-mainform?.addEventListener('submit', () => {
-    console.log("a")
-    let hours: HTMLInputElement | null = document.querySelector('hours-input')
-    let minutes: HTMLInputElement | null = document.querySelector('minutes-input');
-    let seconds: HTMLInputElement | null = document.querySelector('seconds-input');
-
-    if (
-        hours?.valueAsNumber === undefined ||
-        minutes?.valueAsNumber === undefined ||
-        seconds?.valueAsNumber === undefined
-    ) { return; }
-
-    let h_sec: number | undefined = hours.valueAsNumber * 60 * 60;
-    let m_sec: number | undefined = hours.valueAsNumber * 60;
-    let s_sec: number | undefined = hours.valueAsNumber;
-
-    if (h_sec === undefined) {
-        h_sec = 0;
+    to_seconds(): number {
+        return this.hours * 60 * 60 + this.minutes * 60 + this.seconds;
     }
-    
-    if (m_sec === undefined) {
-        m_sec = 0;
-    }
+}
 
-    if (s_sec === undefined) {
-        s_sec = 0;
+let form: HTMLFormElement | null = document.querySelector('#main-form');
+
+form?.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    if (hTxt?.valueAsNumber === undefined) {
+        return;
+    }
+    if (mTxt?.valueAsNumber === undefined) {
+        return;
+    }
+    if (sTxt?.valueAsNumber === undefined) {
+        return;
     }
 
-    let total_time: number = h_sec + m_sec + s_sec
+    let duration: Time = new Time(
+        hTxt.valueAsNumber,
+        mTxt.valueAsNumber,
+        sTxt.valueAsNumber
+    );
 
-    console.log(h_sec);
-    console.log(m_sec);
-    console.log(s_sec);
-    console.log(total_time);
+    let folderPath: string | string[] | null = musicFolder;
+
+    if (folderPath === null || folderPath === undefined) {
+        return;
+    }
+
+    let maincontent: HTMLElement | null = document.querySelector('.container');
+    let loader: HTMLElement | null = document.querySelector('#loader');
+
+    if (maincontent) {
+        maincontent.style.display = 'none';
+    }
+    if (loader) {
+        loader.style.display = 'grid';
+    }
+
+    setTimeout(() => {
+        invoke('get_playlist', {
+            time: duration.to_seconds(),
+            path: folderPath,
+        }).then((vect) => {
+            console.log(vect);
+        });
+    }, 500);
 });
